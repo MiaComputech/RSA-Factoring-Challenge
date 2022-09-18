0dfac0c782b15a3dd31f9af19d9807481296e674
MiaComputech <MiaComputechplc@gmail.com>
2022-09-18 19:59:09 +0300

    Task 1

--- factors.py
+++ factors.py
@@ -1,21 +1,22 @@
-#/usr/bin/env python3
+#!/usr/bin/python3
+#import pdb; pdb.set_trace()
+import math
 import sys
 
+filename = sys.argv[1]
 
-def primef(n):
-    if n <= 3:
-        return int(n)
-    if n % 2 == 0:
-        return 2
-    elif n % 3 == 0:
-        return 3
-    else:
-        for i in range(5, int((n)**0.5) + 1, 6):
-            if n % i == 0:
-                return int(i)
-            if n % (i + 2) == 0:
-                return primef(n/(i+2))
-    return int(n)
-
-
-print(primef(int(sys.argv[1])))
+with open(filename, 'r', encoding='utf-8') as f:
+    a = f.read()
+    a_l = a.count("\n")
+lineas = 0
+b = 0
+with open(filename, 'r', encoding='utf-8') as f:
+    while lineas < a_l:
+        n1 = int (f.readline())
+        lineas += 1
+        a = 2
+        while a < n1//2:
+            if math.fmod(n1, a) == 0:
+                print ("{:d} = {:d} * {:d}".format(n1, a, int(n1/a), end=""))
+                a = n1
+            a += 1
